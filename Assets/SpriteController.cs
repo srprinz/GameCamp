b8043d301aae5001649c2748392c933354787db6
srprinz <srprinz@gmail.com>
2020-12-21 12:04:40 -0500

    egg animations

---
+++ Assets/SpriteController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteController : MonoBehaviour
+{
+    float TimeInterval ;
+    // Start is called before the first frame update
+    void Start()
+    {        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    TimeInterval += Time.deltaTime;
+
+                if (0.5 <= TimeInterval && TimeInterval < 1.0){
+                    if(Input.anyKeyDown){
+                        Vector3 position = this.transform.position;
+                        position.x++;
+                        this.transform.position = position;
+                        TimeInterval = 0;
+                    }
+                }
+                else{  
+                    if(Input.anyKeyDown){
+                        Vector3 position = this.transform.position;
+                        position.x--;
+                        this.transform.position = position;
+                        TimeInterval = 0;
+                    }
+                }
+           // }
+        
+    }
+}
