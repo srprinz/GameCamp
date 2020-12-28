1eeee986d00e06ff1b62a1e2a677aa2b67be5ea3
srprinz <srprinz@gmail.com>
2020-12-28 15:30:46 -0700

    rolling egg added

--- Assets/SpriteController.cs
+++ Assets/SpriteController.cs
@@ -5,18 +5,29 @@ using UnityEngine;
 public class SpriteController : MonoBehaviour
 {
     float TimeInterval ;
+    
+    Animator anim;   
     // Start is called before the first frame update
     void Start()
     {        
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-    TimeInterval += Time.deltaTime;
+        Vector3 minScreenBounds = Camera.main.ScreenToWorldPoint(new Vector3(0, 0, 0));
+    	Vector3 maxScreenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
+        TimeInterval += Time.deltaTime;
+        anim.SetBool("isWalking", false);
+        anim.SetBool("isWalkingBack", false);
+        anim.SetBool("isIdle", true);
 
                 if (0.5 <= TimeInterval && TimeInterval < 1.0){
                     if(Input.anyKeyDown){
+                        anim.SetBool("isWalking", true);
+                        anim.SetBool("isWalkingBack", false);
+                        anim.SetBool("isIdle", false);
                         Vector3 position = this.transform.position;
                         position.x++;
                         this.transform.position = position;
@@ -25,13 +36,18 @@ public class SpriteController : MonoBehaviour
                 }
                 else{  
                     if(Input.anyKeyDown){
+                        anim.SetBool("isWalkingBack", true);
+                        anim.SetBool("isWalking", false);
+                        anim.SetBool("isIdle", false);
                         Vector3 position = this.transform.position;
                         position.x--;
                         this.transform.position = position;
                         TimeInterval = 0;
+                        
                     }
-                }
-           // }
+                } 
         
+		transform.position = new Vector3(Mathf.Clamp(transform.position.x, -10, 75), transform.position.y, transform.position.z);
     }
+    
 }
