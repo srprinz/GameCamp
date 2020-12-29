70db14e0617716bbbfc7e944119070870c562844
Matt Lee <mplee@uno.edu>
2020-12-29 13:38:11 -0600

    Added Single Squack sound and started play/pause script

---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    
+    public Button pauseButton;
+
+
+    // Gets music for Conductor class
+    //public AudioSource musicSource; 
+    AudioSource musicSource = GameObject.Find("musicSource").GetComponent<AudioSource>();
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+        //Listens for a button click
+        pauseButton.onClick.AddListener(TaskOnClick);
+    }
+
+    // On click, pause if music is playing, play if not
+    void TaskOnClick()
+    {
+        if (musicSource.isPlaying)
+        {
+            musicSource.Pause();
+        }
+        else
+        {
+            musicSource.Play();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
