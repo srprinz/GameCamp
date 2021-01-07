cbf991706ca220200e05a55339e7044026267a7a
srprinz <45785219+srprinz@users.noreply.github.com>
2021-01-07 15:30:10 -0600

    fixed input matching

--- Assets/Scripts/SpriteController.cs
+++ Assets/Scripts/SpriteController.cs
@@ -25,7 +25,68 @@ public class SpriteController : MonoBehaviour
         anim.SetBool("isWalkingBack", false);
         anim.SetBool("isIdle", true);
 
-                if (0.5 <= TimeInterval && TimeInterval < 1.0){
+                if ((8.0 <= TimeInterval && TimeInterval < 8.5)||
+                (13.333 <= TimeInterval && TimeInterval < 13.833)||
+                (14.667 <= TimeInterval && TimeInterval < 15.167)||
+                (19.333 <= TimeInterval && TimeInterval < 19.833)||
+                (20.667 <= TimeInterval && TimeInterval < 21.167)||
+                (24.0 <= TimeInterval && TimeInterval < 24.5)||
+                (24.667 <= TimeInterval && TimeInterval < 25.167)||
+                (25.333 <= TimeInterval && TimeInterval < 25.833)||
+                (29.333 <= TimeInterval && TimeInterval < 29.833)||
+                (30.667 <= TimeInterval && TimeInterval < 31.167)||
+                (31.333 <= TimeInterval && TimeInterval < 31.833)||
+                (36.0 <= TimeInterval && TimeInterval < 36.5)||
+                (38.333 <= TimeInterval && TimeInterval < 38.833)||
+                (39.0 <= TimeInterval && TimeInterval < 39.5)||
+                (45.333 <= TimeInterval && TimeInterval < 45.833)||
+                (47.333 <= TimeInterval && TimeInterval < 47.833)||
+                (51.333 <= TimeInterval && TimeInterval < 51.833)||
+                (56.667 <= TimeInterval && TimeInterval < 57.167)||
+                (57.333 <= TimeInterval && TimeInterval < 57.833)||
+                (58.0 <= TimeInterval && TimeInterval < 58.5)||
+                (66.667 <= TimeInterval && TimeInterval < 67.167)||
+                (67.103 <= TimeInterval && TimeInterval < 67.603)||
+                (67.333 <= TimeInterval && TimeInterval < 67.833)||
+                (68.0 <= TimeInterval && TimeInterval < 68.5)||
+                (68.667 <= TimeInterval && TimeInterval < 69.167)||
+                (72.0 <= TimeInterval && TimeInterval < 72.5)||
+                (72.667 <= TimeInterval && TimeInterval < 73.167)||
+                (73.333 <= TimeInterval && TimeInterval < 73.833)||
+                (73.769 <= TimeInterval && TimeInterval < 74.269)||
+                (74.0 <= TimeInterval && TimeInterval < 74.5)||
+                (77.333 <= TimeInterval && TimeInterval < 77.833)||
+                (78.0 <= TimeInterval && TimeInterval < 78.5)||
+                (78.667 <= TimeInterval && TimeInterval < 79.167)||
+                (79.333 <= TimeInterval && TimeInterval < 79.833)||
+                (79.769 <= TimeInterval && TimeInterval < 80.269)||
+                (82.667 <= TimeInterval && TimeInterval < 83.167)||
+                (83.333 <= TimeInterval && TimeInterval < 83.833)||
+                (83.769 <= TimeInterval && TimeInterval < 84.769)||
+                (88.0 <= TimeInterval && TimeInterval < 88.5)||
+                (88.436 <= TimeInterval && TimeInterval < 88.936)||
+                (88.667 <= TimeInterval && TimeInterval < 89.167)||
+                (89.333 <= TimeInterval && TimeInterval < 89.833)||
+                (94.436 <= TimeInterval && TimeInterval < 94.936)||
+                (95.333 <= TimeInterval && TimeInterval < 95.833)||
+                (98.667 <= TimeInterval && TimeInterval < 99.167)||
+                (99.769 <= TimeInterval && TimeInterval < 100.269)||
+                (100.0<= TimeInterval && TimeInterval < 100.5)||
+                (101.103 <= TimeInterval && TimeInterval < 101.603)||
+                (104.436 <= TimeInterval && TimeInterval < 104.936)||
+                (104.667 <= TimeInterval && TimeInterval < 104.167)||
+                (105.333 <= TimeInterval && TimeInterval < 105.833)||
+                (106.0 <= TimeInterval && TimeInterval < 106.5)||
+                (109.769 <= TimeInterval && TimeInterval < 110.269)||
+                (110.0 <= TimeInterval && TimeInterval < 110.5)||
+                (111.103 <= TimeInterval && TimeInterval < 111.603)||
+                (111.333 <= TimeInterval && TimeInterval < 111.833)||
+                (114.667 <= TimeInterval && TimeInterval < 115.167)||
+                (115.333 <= TimeInterval && TimeInterval < 115.833)||
+                (115.769 <= TimeInterval && TimeInterval < 116.269)||
+                (116.667 <= TimeInterval && TimeInterval < 117.167)||
+                (117.103 <= TimeInterval && TimeInterval < 117.603)
+                ){
                     if(Input.anyKeyDown){
                         anim.SetBool("isWalking", true);
                         anim.SetBool("isWalkingBack", false);
@@ -34,7 +95,7 @@ public class SpriteController : MonoBehaviour
                         position.x++;
                         this.transform.position = position;
                         score += (int)(TimeInterval*20);
-                        TimeInterval = 0;
+                        //TimeInterval = 0;
 
                         print("The score is :"+score);
                         //SetScoreText();
@@ -50,7 +111,7 @@ public class SpriteController : MonoBehaviour
                         this.transform.position = position;
 
                         score -= (int)(TimeInterval*5);
-                        TimeInterval = 0;
+                      //  TimeInterval = 0;
                         print("The score is :"+score);
                     }
                 }
